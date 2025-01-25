7776233be743bfbebd7f201cb6e2af0685fdf13d
starchcode <mrdave67@gmail.com>
2025-01-25 20:04:05 +0000

    converts html safely with santisiser

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { TasksService } from '../services/tasks.service';
 import { selectTasks, selectTasksError } from '../store/tasks.reducer';
 import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tasks',
@@ -16,12 +17,20 @@ export class TasksComponent implements OnInit {
   tasks$: Observable<any>;
   error$: Observable<string | null>;
 
-  constructor(private store: Store, private tasksService: TasksService) {
+  constructor(
+    private store: Store,
+    private tasksService: TasksService,
+    private sanitizer: DomSanitizer
+  ) {
     this.tasks$ = this.store.select(selectTasks);
     this.error$ = this.store.select(selectTasksError);
   }
 
   ngOnInit(): void {
     this.tasksService.fetchTasks();
+  }
+
+  convertToHtml(description: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(description);
   }
 }
