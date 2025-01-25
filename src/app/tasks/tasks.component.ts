bf1091a9b21449d772aaef0e6e1805568e5affc3
starchcode <mrdave67@gmail.com>
2025-01-25 22:58:59 +0000

    shows tasks as list from angular/material

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -5,11 +5,13 @@ import { TasksService } from '../services/tasks.service';
 import { selectTasks, selectTasksError } from '../store/tasks.reducer';
 import { CommonModule } from '@angular/common';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatDividerModule, MatListModule],
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
