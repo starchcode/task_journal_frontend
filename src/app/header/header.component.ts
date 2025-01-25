b600b0089933c73b94fb671741fcfaa82f1e2721
starchcode <mrdave67@gmail.com>
2025-01-25 22:15:36 +0000

    tried out angular material and added a button from library to test component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,11 +3,12 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { selectHeaderTitle, selectHeaderCount, HeaderState, incrementHeaderCount } from '../store/header.reducer';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,  // Mark as standalone
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
